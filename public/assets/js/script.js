8255e81f1498f03f7f6963e408921c2b36c96524
Casey Ferguson <cferg019@gmail.com>
2020-02-08 22:05:07 -0500

    final version

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
-    
-  $(".devour-form").on("submit", function(event) {
+$(document).ready(function () {
+
+  $(".devour-form").on("submit", function (event) {
     event.preventDefault();
 
     var burger_id = $(this).children(".burger_id").val();
@@ -8,10 +8,22 @@ $(document).ready(function() {
     $.ajax({
       method: "PUT",
       url: "/burgers/" + burger_id
-    }).then(function(data) {
+    }).then(function (data) {
       // reload page to display devoured burger in proper column
       location.reload();
     });
 
   });
+
+  $("#burger-submit-form").on("submit", function (event) {
+
+    if ($("#enter-text").val() === "") {
+      event.preventDefault();
+      $("#error").text("Please enter a burger name.")
+    }
+
+
+
+  })
 });
+
